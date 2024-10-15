73aa562b0aecb150c0a9035d00a3504a04e622f9
testing1234567891011121314 <90443963+testing1234567891011121314@users.noreply.github.com>
2024-10-15 16:33:51 +0800

    Add Authentication Test

---
+++ src/test/java/seedu/address/authentication/AuthenticationTest.java
@@ -0,0 +1,8 @@
+package seedu.address.authentication;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static seedu.address.testutil.Assert.assertThrows;
+
+public class AuthenticationTest {
+}
